4de420a873d30966947465a7da3cdf5e2f644017
DiXL0f0S <vladislav-psarov@ya.ru>
2017-06-30 19:49:07 +1000

    FragmentActivity changed to AppCompatActivity

--- VKTestApplication/src/main/java/com/vk/vktestapp/LoginActivity.java
+++ VKTestApplication/src/main/java/com/vk/vktestapp/LoginActivity.java
@@ -2,7 +2,7 @@ package com.vk.vktestapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,7 +18,7 @@ import com.vk.sdk.api.VKError;
  * Activity which displays a login screen to the user, offering registration as
  * well.
  */
-public class LoginActivity extends FragmentActivity {
+public class LoginActivity extends AppCompatActivity {
 
     private boolean isResumed = false;
 
