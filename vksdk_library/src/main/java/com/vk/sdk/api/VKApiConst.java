5dfc2bd7ba43458f6e22308fe37d9167c9ea8680
DiXL0f0S <vladislav-psarov@ya.ru>
2017-10-29 12:38:38 +1000

    VKApiConst.LIMIT

--- vksdk_library/src/main/java/com/vk/sdk/api/VKApiConst.java
+++ vksdk_library/src/main/java/com/vk/sdk/api/VKApiConst.java
@@ -34,6 +34,7 @@ public class VKApiConst {
     public static final String OFFSET = "offset";
     public static final String COUNT = "count";
     public static final String OWNER_ID = "owner_id";
+    public static final String LIMIT = "limit";
 
     //auth
     public static final String VERSION = "v";
